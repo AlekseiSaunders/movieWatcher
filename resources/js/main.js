546dc7b68f1270fa13b31a60850a9536ac14df35
Aleksei Saunders <alekseisaunders@gmail.com>
2022-06-30 08:57:56 -0600

    add javascript to fetch initial array of movies that match search string and then fetch more details based on initial array

---
+++ resources/js/main.js
@@ -0,0 +1,68 @@
+const searchBtn = document.getElementById('movieSearchBtn');
+const searchInput = document.getElementById('movieSearchInput');
+let movieArray = [];
+let movieData = [];
+
+let handleClick = function () {
+  fetch(`http://www.omdbapi.com/?apikey=9e510766&s=${searchInput.value}`)
+    .then(handleResponse)
+    .then((data) => {
+      for (movie of data.Search) {
+        movieArray.push(movie.Title);
+      }
+      return movieArray;
+    })
+    .then((movieArray) => {
+      movieArray.forEach((movie) => {
+        fetch(`http://www.omdbapi.com/?apikey=9e510766&t=${movie}&p=short`)
+          .then(handleResponse)
+          .then((data) => movieData.push(data));
+      });
+      console.log(movieData);
+    })
+    .catch((error) => console.log(error));
+};
+
+function handleResponse(response) {
+  const contentType = response.headers.get('content-type');
+  if (contentType.includes('application/json')) {
+    return handleJSONResponse(response);
+  } else if (contentType.includes('text/html')) {
+    return handleTextResponse;
+  } else {
+    throw new Error(
+      `Sorry, content-type: ${contentType} is not currently supported`
+    );
+  }
+}
+
+function handleJSONResponse(response) {
+  return response.json().then((json) => {
+    if (response.ok) {
+      return json;
+    } else {
+      return Promise.reject(
+        Object.assign({}, json, {
+          status: response.status,
+          statusText: response.statusText,
+        })
+      );
+    }
+  });
+}
+
+function handleTextResponse(response) {
+  return response.text().then((text) => {
+    if (response.ok) {
+      return text;
+    } else {
+      return Promise.reject({
+        status: response.status,
+        statusText: response.statusText,
+        err: text,
+      });
+    }
+  });
+}
+
+searchBtn.addEventListener('click', handleClick);
