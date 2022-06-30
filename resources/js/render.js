39ae65fbdf7faddee81460d2fc8505d3026fd3a0
Aleksei Saunders <alekseisaunders@gmail.com>
2022-06-30 13:17:09 -0600

    add render.js file

--- resources/js/render.js
+++ resources/js/render.js
@@ -1,4 +1,26 @@
-import { movieData, handleClick } from './fetch.js';
-const searchBtn = document.getElementById('movieSearchBtn');
+const mainContent = document.getElementById('main_content');
 
-searchBtn.addEventListener('click', handleClick);
+function renderMovies(movie) {
+  let movieArticle = document.createElement('article');
+  movieArticle.classList.add('movie_article');
+  movieArticle.innerHTML = `
+	<img src="${movie.Poster}">
+  <div class="movie_info">
+    <div class="movie_header">
+	    <h2>${movie.Title}</h2>
+      <p>${movie.Year}</p>
+      <p>${movie.imdbRating}</p>
+	  </div>
+    <div class="movie_details">
+      <p>${movie.Runtime}</p>
+      <p>${movie.Genre}</p>
+      <button>Add</button>
+    </div>
+    <div class="movie_plot">
+			<p>${movie.Plot}</p>
+  </div>
+`;
+  mainContent.appendChild(movieArticle);
+}
+
+export { renderMovies };
